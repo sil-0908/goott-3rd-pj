b40a969cf36bb6c9366d5204c55d9a73cf77d6c3
장재호 <diehreo@gmail.com>
2023-04-03 22:32:14 +0900

    setting-0403-3

--- 3rdpj/src/main/java/com/goott/pj3/common/util/AlertHandler.java
+++ 3rdpj/src/main/java/com/goott/pj3/common/util/AlertHandler.java
@@ -1,5 +1,28 @@
 package com.goott.pj3.common.util;
 
-public class AlertHandler {
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Component
+@RequestMapping("/alert-ws")
+public class AlertHandler extends TextWebSocketHandler {
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		
+	}
 }
