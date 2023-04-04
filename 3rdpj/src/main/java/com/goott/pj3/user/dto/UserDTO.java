d7e7cae45755b04594c655292ee1b1290a5d8947
sil-0908 <alstlf2z@nate.com>
2023-04-04 18:46:36 +0900

    230404 장민실 user ver 1.0

--- 3rdpj/src/main/java/com/goott/pj3/user/dto/UserDTO.java
+++ 3rdpj/src/main/java/com/goott/pj3/user/dto/UserDTO.java
@@ -1,5 +1,65 @@
 package com.goott.pj3.user.dto;
 
 public class UserDTO {
+	
+	String user_id, pw, birth, email, hp, auth;
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}	
+	
+	public String getAuth() {
+		return auth;
+	}
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+
+	@Override
+	public String toString() {
+	return "UserDTO [user_id = " + user_id
+			+ ", pw = " + pw
+			+ ", birth = " + birth
+			+ ", email = " + email
+			+ ", hp = " + hp
+			+ ", auth = " + auth + "]";
+	}
 
 }
--- 3rdpj/src/main/java/com/goott/pj3/user/repo/UserDAO.java
+++ 3rdpj/src/main/java/com/goott/pj3/user/repo/UserDAO.java
@@ -4,10 +4,32 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.goott.pj3.user.dto.UserDTO;
+
 @Repository
 public class UserDAO {
 
 	@Autowired
 	SqlSession ss;
+
+//	로그인할때 일치하는 유저정보 있는지 int 반환
+	public int user_cnt(UserDTO u_dto) {
+		return ss.selectOne("user.user_cnt", u_dto);
+	}
+
+//	세션 저장시킬 유저정보 dto담아오기
+	public UserDTO find_user(String id) {
+		return ss.selectOne("user.find_user", id);
+	}
+
+//	회원가입
+	public void sign_up(UserDTO u_dto) {
+		ss.insert("user.sign_up", u_dto);
+	}
+
+//	아이디 중복체크
+	public int id_chk(String id) {
+		return ss.selectOne("user.id_chk", id);
+	}
 	
 }
