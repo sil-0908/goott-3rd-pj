34d5ede671f82d22ed2e0630ffe9a5c84f7c0afd
MarinKr <youngjoongil@gmail.com>
2023-04-23 19:47:06 +0900

    2023.04.23

--- 3rdpj/src/main/java/com/goott/pj3/plan/controller/PaymentController.java
+++ 3rdpj/src/main/java/com/goott/pj3/plan/controller/PaymentController.java
@@ -6,6 +6,7 @@ import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,14 +20,19 @@ import java.util.Locale;
 public class PaymentController {
     private final IamportClient iamportClient;
 
+    private String ai = "1003071753880806";
+
+    private String sec = "US46uhT0JGWE7fGoxqtb3tVOEF1UCelTe5xD210RuLLOaUxWBtWmUHmdM1VcTM6lMgpaBr08R4wRBdLH";
+
     private final PaymentService paymentService;
 
     public PaymentController(IamportClient iamportClient, PaymentService paymentService) {
-        this.iamportClient = iamportClient;
+        this.iamportClient = new IamportClient(ai,sec);
         this.paymentService = paymentService;
     }
 
-    @PostMapping("/payment/verify/{imp_uid}")
+    @PostMapping("/verifyiamport/{imp_uid}")
+    @ResponseBody
     public IamportResponse<Payment> paymentByUid(Model model, Locale locale, HttpSession httpSession,
                                                  @PathVariable(value = "imp_uid") String imp_uid) throws IamportResponseException, IOException {
         return iamportClient.paymentByImpUid(imp_uid);
