59289daf921eb774a63e56a60811aa5fcc8e80dc
조원재 <hadnjswp@gmail.com>
2023-04-27 01:17:34 +0900

    여행정보 게시판 이미지 업로드 포함하여 CRUD 수정

--- 3rdpj/src/main/java/com/goott/pj3/travelinfo/repo/TravelInfoDAO.java
+++ 3rdpj/src/main/java/com/goott/pj3/travelinfo/repo/TravelInfoDAO.java
@@ -1,39 +1,63 @@
-//package com.goott.pj3.travelinfo.repo;
-//
-//import org.apache.ibatis.session.SqlSession;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Repository;
-//
-//import com.goott.pj3.common.util.paging.Criteria;
-//import com.goott.pj3.travelinfo.dto.TravelInfoDTO;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//@Repository
-//public class TravelInfoDAO {
-//
-//	@Autowired
-//	SqlSession ss;
-//
-//	/**
-//	 * 조원재 23.04.08. 여행지 정보 리스트
-//	 * @param
-//	 * @return
-//	 */
-//	public List<TravelInfoDTO> list(Criteria cri) {
-//		return ss.selectList("travelinfo.list", cri);
-//	}
-//
-//	public int totalCount(Criteria cri) {
-//		return ss.selectOne("travelinfo.totalCount", cri);
-//	}
-//
-//    public int create(TravelInfoDTO travelInfoDTO) {
-//		return this.ss.insert("travelinfo.insert", travelInfoDTO);
-//    }
-//
-//	public TravelInfoDTO detail(TravelInfoDTO travelInfoDTO) {
-//		return this.ss.selectOne("travelinfo.detail", travelInfoDTO);
-//	}
-//}
+package com.goott.pj3.travelinfo.repo;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.goott.pj3.common.util.paging.Criteria;
+import com.goott.pj3.travelinfo.dto.TravelInfoDTO;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class TravelInfoDAO {
+
+    @Autowired
+    SqlSession ss;
+
+    /**
+     * 조원재 23.04.08. 여행지 정보 리스트
+     * @param
+     * @return
+     */
+    public List<TravelInfoDTO> list(Criteria cri) {
+        return ss.selectList("travelinfo.list", cri);
+    }
+
+    public int totalCount(Criteria cri) {
+        return ss.selectOne("travelinfo.totalCount", cri);
+    }
+
+    public int create(TravelInfoDTO travelInfoDTO) {
+        return this.ss.insert("travelinfo.insert");
+    }
+
+    public void createImg(TravelInfoDTO travelInfoDTO) {
+        this.ss.insert("travelinfo.insertfile", travelInfoDTO);
+    }
+
+    public TravelInfoDTO detail(TravelInfoDTO travelInfoDTO) {
+        return this.ss.selectOne("travelinfo.detail", travelInfoDTO);
+    }
+
+    public int update(TravelInfoDTO travelInfoDTO) {
+        return this.ss.update("travelinfo.update", travelInfoDTO);
+    }
+
+    public void deleteImg(TravelInfoDTO travelInfoDTO) {
+        this.ss.delete("travelinfo.deleteImg", travelInfoDTO);
+    }
+
+    public void updateImg(TravelInfoDTO travelInfoDTO) {
+        this.ss.insert("travelinfo.updateImg", travelInfoDTO);
+    }
+
+    public int delete(TravelInfoDTO travelInfoDTO) {
+        return this.ss.update("travelinfo.delete", travelInfoDTO);
+    }
+
+    public List<TravelInfoDTO> imgList(TravelInfoDAO travelInfoDAO) {
+        return this.ss.selectList("travelinfo.imgList", travelInfoDAO);
+    }
+}
--- 3rdpj/src/main/java/com/goott/pj3/travelinfo/service/TravelInfoServiceImpl.java
+++ 3rdpj/src/main/java/com/goott/pj3/travelinfo/service/TravelInfoServiceImpl.java
@@ -1,52 +1,87 @@
-//package com.goott.pj3.travelinfo.service;
-//
-//import com.goott.pj3.common.util.paging.Criteria;
-//import com.goott.pj3.common.util.paging.PagingDTO;
-//import com.goott.pj3.travelinfo.dto.TravelInfoDTO;
-//import com.goott.pj3.travelinfo.repo.TravelInfoDAO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//@Service
-//public class TravelInfoServiceImpl implements TravelInfoService {
-//
-//	@Autowired
-//	TravelInfoDAO travelInfoDAO;
-//
-//	/**
-//	 * 조원재 23.04.08. 여행지 정보 리스트;
-//	 * @param
-//	 * @return
-//	 */
-//	@Override
-//	public List<TravelInfoDTO> list(Criteria cri) {
-//		// TODO Auto-generated method stub
-//		return travelInfoDAO.list(cri);
-//	}
-//
-//	@Override
-//	public PagingDTO paging(Criteria cri) {
-//		PagingDTO paging = new PagingDTO();
-//		paging.setCri(cri);
-//		paging.setTotalCount(travelInfoDAO.totalCount(cri));
-//		return paging;
-//	}
-//
-//	@Override
-//	public int create(TravelInfoDTO travelInfoDTO) {
-//		int cnt = this.travelInfoDAO.create(travelInfoDTO);
-//		if (cnt==1){
-//			System.out.println("return idx : " + travelInfoDTO.getTravel_location_idx());
-//			return travelInfoDTO.getTravel_location_idx();
-//		}
-//		return 0;
-//	}
-//
-//	@Override
-//	public TravelInfoDTO detail(TravelInfoDTO travelInfoDTO) {
-//		return this.travelInfoDAO.detail(travelInfoDTO);
-//	}
-//}
+package com.goott.pj3.travelinfo.service;
+
+import com.goott.pj3.common.util.paging.Criteria;
+import com.goott.pj3.common.util.paging.PagingDTO;
+import com.goott.pj3.travelinfo.dto.TravelInfoDTO;
+import com.goott.pj3.travelinfo.repo.TravelInfoDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class TravelInfoServiceImpl implements TravelInfoService {
+
+    @Autowired
+    TravelInfoDAO travelInfoDAO;
+
+    /**
+     * 조원재 23.04.08. 여행지 정보 리스트;
+     * @param
+     * @return
+     */
+    @Override
+    public List<TravelInfoDTO> list(Criteria cri) {
+        // TODO Auto-generated method stub
+        return travelInfoDAO.list(cri);
+    }
+
+    @Override
+    public PagingDTO paging(Criteria cri) {
+        PagingDTO paging = new PagingDTO();
+        paging.setCri(cri);
+        paging.setTotalCount(travelInfoDAO.totalCount(cri));
+        return paging;
+    }
+
+    @Override
+    public int create(TravelInfoDTO travelInfoDTO) {
+        int affectRowCnt = this.travelInfoDAO.create(travelInfoDTO);
+        if(affectRowCnt==1){
+            return travelInfoDTO.getTravel_location_idx();
+        }
+        return 0;
+    }
+
+    @Override
+    public void createImg(TravelInfoDTO travelInfoDTO) {
+        this.travelInfoDAO.createImg(travelInfoDTO);
+    }
+
+    @Override
+    public TravelInfoDTO detail(TravelInfoDTO travelInfoDTO) {
+        return this.travelInfoDAO.detail(travelInfoDTO);
+    }
+
+    @Override
+    public int update(TravelInfoDTO travelInfoDTO) {
+        int cnt = this.travelInfoDAO.update(travelInfoDTO);
+        if(cnt==1){
+            return travelInfoDTO.getTravel_location_idx();
+        }
+        return 0;
+    }
+
+    @Override
+    public void deleteImg(TravelInfoDTO travelInfoDTO) {
+        this.travelInfoDAO.deleteImg(travelInfoDTO);
+    }
+
+    @Override
+    public void updateImg(TravelInfoDTO travelInfoDTO) {
+        this.travelInfoDAO.updateImg(travelInfoDTO);
+    }
+
+    @Override
+    public boolean delete(TravelInfoDTO travelInfoDTO) {
+        int cnt = this.travelInfoDAO.delete(travelInfoDTO);
+        return cnt==1;
+    }
+
+    @Override
+    public List<TravelInfoDTO> imglist(TravelInfoDTO travelInfoDTO) {
+        return this.travelInfoDAO.imgList(travelInfoDAO);
+    }
+
+}
